5b33fade1a70ac5d28e3996fe9fd628a81c05020
Eduardo J. Brenes Perez <edubpz08@gmail.com>
2026-03-10 11:20:59 -0600

    AccountController has been created.

---
+++ AsadaLisboaBackend/Controllers/AccountController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AsadaLisboaBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+    }
+}
