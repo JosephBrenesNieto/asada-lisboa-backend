03327208c6c77ab2c50d35a1a71fdd75279609d4
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-13 18:19:08 -0600

    delete class unnecessary, renaiming class RegisterUserDTO,

--- AsadaLisboaBackend.Models/DTOs/Account/RegisterRequestDTO.cs
+++ AsadaLisboaBackend.Models/DTOs/Account/RegisterRequestDTO.cs
@@ -5,29 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
-namespace AsadaLisboaBackend.Models.DTOs
+namespace AsadaLisboaBackend.Models.DTOs.Account
 {
-    public class RegisterUserDTO
+    public class RegisterRequestDTO
     {
         [Required(ErrorMessage = "Nombre de usuario requerido")]
-        [DataType(DataType.Text)]
         public string UserName { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Email es requerido")]
         [EmailAddress(ErrorMessage = "Formato incorrecto para un email")]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$", ErrorMessage = "La contraseña debe incluir mayúsculas, minúsculas y números.")]
-        [DataType(DataType.EmailAddress)]
         public string Email { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Contraseña es requerida")]
-        [DataType(DataType.Password)]
         public string Password { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Confirmar Contraseña es requerida")]
         [StringLength(100, MinimumLength = 8, ErrorMessage = "La contraseña debe tener al menos 8 caracteres.")]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$", ErrorMessage = "La contraseña debe incluir mayúsculas, minúsculas y números.")]
-        [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = "Should macth with the Password")]
-        public string ConfirnPassword { get; set; } = string.Empty;
+        [Compare("Password", ErrorMessage = "Debe considir con la contraseña")]
+        public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
