c368575a2e933a407b7226f5b7514aab78cbf6a0
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-13 20:56:10 -0600

    Add services

---
+++ AsadaLisboaBackend.Services/Account/RegisterUserService.cs
@@ -0,0 +1,46 @@
+﻿using AsadaLisboaBackend.Models.DTOs.Account;
+using AsadaLisboaBackend.Models.IdentityModels;
+using AsadaLisboaBackend.ServiceContracts.Account;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsadaLisboaBackend.Services.Account
+{
+    public class RegisterUserService : IRegisterUserService
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegisterUserService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        public async Task<IdentityResult> RegisterUser(RegisterRequestDTO dto)
+        {
+            //Verify if eamil exists
+            var existinguser = await _userManager.FindByEmailAsync(dto.Email);
+
+            if (existinguser != null)
+                return BadRequest("El correo electrónico ya esta registrado ");
+
+            //Register new user
+
+            var user = new ApplicationUser
+            {
+
+                UserName = dto.UserName,
+                Email = dto.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, dto.Password);
+
+            return result;
+        }
+    }
+}
