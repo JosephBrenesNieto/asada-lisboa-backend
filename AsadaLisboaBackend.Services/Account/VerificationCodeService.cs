866dd83c15dbb86d04d34445942b9d66562049b6
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-18 16:58:39 -0600

    Modified VerificationCodeService

--- AsadaLisboaBackend.Services/Account/VerificationCodeService.cs
+++ AsadaLisboaBackend.Services/Account/VerificationCodeService.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AsadaLisboaBackend.Models.DTOs.Account;
 using AsadaLisboaBackend.Models.IdentityModels;
 using AsadaLisboaBackend.ServiceContracts.Account;
 using AsadaLisboaBackend.Services.Email;
 using Microsoft.AspNetCore.Identity;
-using AsadaLisboaBackend.Models.DTOs.Account;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AsadaLisboaBackend.Services.Account
 {
@@ -27,12 +30,26 @@ namespace AsadaLisboaBackend.Services.Account
             var user = await _userManager.FindByEmailAsync(email);
             if (user is null) return false;
 
+            var generateToken = await _userManager.GenerateEmailConfirmationTokenAsync(user);
 
-            var random = new Random();
-            var code = random.Next(100000, 999999).ToString();
+            var encodedToken = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(generateToken));
+
 
             //Send email with code 
-            return await _verificationCodeSendService.SendVerificationCode(user.FirstName, email, code);
+            return await _verificationCodeSendService.SendVerificationCode(user.FirstName, email, encodedToken);
+        }
+
+        public async Task<bool> ConfirmEmailAsync(string email, string token)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user is null) return false;
+
+            // Decodificar token
+            var decodedToken = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(token));
+
+            var result = await _userManager.ConfirmEmailAsync(user, decodedToken);
+            return result.Succeeded;
+
         }
     }
 }
