54c8466adecbd87265cc6f307b1d5979ce7f0e5e
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-18 16:53:30 -0600

    Modified class verificationCodeRequestDTO

--- AsadaLisboaBackend.Models/DTOs/Account/VerificationCodeRequestDTO.cs
+++ AsadaLisboaBackend.Models/DTOs/Account/VerificationCodeRequestDTO.cs
@@ -13,10 +13,12 @@ namespace AsadaLisboaBackend.Models.DTOs.Account
         public int UserId { get; set; }
         public string Email { get; set; } = string.Empty;
 
-        public string Code { get; set; } = string.Empty;
-        public DateTime Expiration { get; set; }
-        public bool IsUsed { get; set; }
-        public int ResendCount { get; set; } = 0;   // contador de reenvíos
-        public DateTime? LastSent { get; set; }      // última vez que se envió
+        public string Token { get; set; } = string.Empty;
+
+        //public string Code { get; set; } = string.Empty;
+        //public DateTime Expiration { get; set; }
+        //public bool IsUsed { get; set; }
+        //public int ResendCount { get; set; } = 0;   // contador de reenvíos
+        //public DateTime? LastSent { get; set; }      // última vez que se envió
     }
 }
