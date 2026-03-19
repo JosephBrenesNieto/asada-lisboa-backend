1af4970092bda6ddd512eaf1bb6b7febb62e2717
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-19 11:50:06 -0600

    Add classes Services

---
+++ AsadaLisboaBackend.ServiceContracts/Image/IImageService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AsadaLisboaBackend.Models.DTOs.Image;
+
+namespace AsadaLisboaBackend.ServiceContracts.Image
+{
+    public interface IImageService
+    {
+        public Task<ImageResponseDTO> CreateImage(ImageRequestDTO imageRequestDTO);
+    }
+}
---
+++ AsadaLisboaBackend.Services/Image/ImageService.cs
@@ -0,0 +1,59 @@
+﻿using AsadaLisboaBackend.ServiceContracts.Image;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AsadaLisboaBackend.Models.DTOs.Image;
+using AsadaLisboaBackend.Models.DatabaseContext;
+using AsadaLisboaBackend.Models;
+
+
+namespace AsadaLisboaBackend.Services.Image
+{
+    public class ImageService : IImageService
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public ImageService(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<ImageResponseDTO> CreateImage(ImageRequestDTO imageRequestDTO)
+        {
+           
+            var newImage = new Image
+            { 
+                ID = Guid.NewGuid(),
+                Title = imageRequestDTO.Title,
+                Description = imageRequestDTO.Description,
+                Slug = imageRequestDTO.Title.ToLower().Replace(" ", "-"),                
+                PublicationDate = DateTime.UtcNow,
+                FileSize = imageRequestDTO.FileSize,
+                StatusId = imageRequestDTO.StatusId,
+                Categories = _applicationDbContext.Categories
+                .Where(c => imageRequestDTO.CategoryIds.Contains(c.Id))
+                .ToList()
+            };
+
+            _applicationDbContext.Images.Add(newImage);
+            await _applicationDbContext.SaveChangesAsync();
+
+            return new ImageResponseDTO
+            {
+                Id = newImage.Id,
+                Slug = newImage.Slug,
+                Title = newImage.Title,
+                Description = newImage.Description,
+                PublicationDate = newImage.PublicationDate,
+                FileSize = newImage.FileSize,
+                StatusName = newImage.Status?.Name ?? string.Empty,
+                Categories = newImage.Categories.Select(c => c.Name).ToList()
+            };
+
+
+        }
+
+    }
+}
