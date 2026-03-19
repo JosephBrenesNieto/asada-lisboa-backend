81ef2bf0ae27860c766e928e4ee9969d19b93239
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-19 17:06:53 -0600

    Add ImageUpdateRequestDTO.cs

---
+++ AsadaLisboaBackend.Models/DTOs/Image/ImageUpdateRequestDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsadaLisboaBackend.Models.DTOs.Image
+{
+    public class ImageUpdateRequestDTO
+    {
+        public Guid Id { get; set; }              // Identificador de la imagen
+        public string Title { get; set; } = "";   // Nuevo título
+        public string Description { get; set; } = "";
+        public Guid StatusId { get; set; }
+        public List<Guid> CategoryIds { get; set; } = new();
+        //public IFormFile? File { get; set; }      // Opcional: nueva imagen
+
+    }
+}
