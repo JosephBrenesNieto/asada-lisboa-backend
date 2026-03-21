7ee70aebb2f0c65ee8713ed186294299a743b9e6
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-20 20:51:23 -0600

    Modified ImageUpdateRequestDTO

--- AsadaLisboaBackend.Models/DTOs/Image/ImageUpdateRequestDTO.cs
+++ AsadaLisboaBackend.Models/DTOs/Image/ImageUpdateRequestDTO.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AsadaLisboaBackend.Utils.ImageAttribute;
+using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +11,26 @@ namespace AsadaLisboaBackend.Models.DTOs.Image
 {
     public class ImageUpdateRequestDTO
     {
-        public Guid Id { get; set; }              // Identificador de la imagen
-        public string Title { get; set; } = "";   // Nuevo título
-        public string Description { get; set; } = "";
+        public Guid Id { get; set; }
+
+
+        [Required(ErrorMessage = "El titulo es requerido")]
+        [StringLength(50)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La descripción es requerido")]
+        [StringLength(100)]
+        public string Description { get; set; } = string.Empty;
+
+        [AllowedExtensions(new string[] { ".jpg", ".jpeg", ".png" })]
+        [MaxFileSize(5)] // Límite de 5 MB
+        public IFormFile File { get; set; } = null!;
+
+        [Range(1, int.MaxValue, ErrorMessage = "Debe selecionar un estado válido")]
         public Guid StatusId { get; set; }
+
         public List<Guid> CategoryIds { get; set; } = new();
-        //public IFormFile? File { get; set; }      // Opcional: nueva imagen
+        
 
     }
 }
