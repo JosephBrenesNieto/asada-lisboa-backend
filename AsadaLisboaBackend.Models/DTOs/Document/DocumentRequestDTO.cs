336d5e2997e74a0e5cf8af3877363b307e757e29
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-23 16:16:54 -0600

    Add DocumentRequestDTO and DocumentResponseDTO

---
+++ AsadaLisboaBackend.Models/DTOs/Document/DocumentRequestDTO.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using AsadaLisboaBackend.Utils.DocumentAtribute;
+
+namespace AsadaLisboaBackend.Models.DTOs.Document
+{
+    public class DocumentRequestDTO
+    {
+        [Required(ErrorMessage = "El titulo es requerido")]
+        [StringLength(50)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La descripción es requerida")]
+        [StringLength(100)]
+        public string Description { get; set; } = string.Empty;
+
+
+        [AllowedExtensions(new string[] { ".pdf", ".docx", ".xlsx" })]
+        [MaxFileSize(5)] // Límite de 5 MB
+        public IFormFile File { get; set; } = null!;
+    }
+}
---
+++ AsadaLisboaBackend.Models/DTOs/Document/DocumentResponseDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsadaLisboaBackend.Models.DTOs.Document
+{
+    public class DocumentResponseDTO
+    {
+       public Guid Id { get; set; }
+
+       public string Slug { get; set; } = string.Empty;
+       public string Title { get; set; } = string.Empty;
+
+       public string Description { get; set; } = string.Empty;
+
+        public DateTime PublicationDate;
+
+        public long FileSize;
+
+      public string StatusName { get; set; } = string.Empty;
+
+        public string DocumentTypeName { get; set; } = string.Empty;
+
+        public List<string> Categories { get; set; } = new();
+    }
+}
