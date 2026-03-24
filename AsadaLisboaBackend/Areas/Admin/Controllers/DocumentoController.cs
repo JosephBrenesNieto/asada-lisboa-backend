675d512265e7f77e4c6e9716e9bf0b2fc81d8587
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-24 15:32:20 -0600

    Add DocumentoController

---
+++ AsadaLisboaBackend/Areas/Admin/Controllers/DocumentoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AsadaLisboaBackend.ServiceContracts.Document;
+using AsadaLisboaBackend.Models.DTOs.Document;
+using AsadaLisboaBackend.Utils.OptionsPattern;
+
+namespace AsadaLisboaBackend.Areas.Admin.Controllers
+{
+    [ApiController]
+    [Area("Admin")]
+    [Route("api/[area]/[controller]")]
+    public class DocumentoController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IDocumentService _documentService;
+
+        public DocumentoController(IWebHostEnvironment env, IDocumentService documentService)
+        {
+            _env = env;
+            _documentService = documentService;
+        }
+
+
+
+        [HttpPost("")]
+        public async Task<IActionResult> CreateImage([FromForm] DocumentRequestDTO documentRequestDTO)
+        {
+            var options = new FileStorageOptions
+            {
+                BasePath = Path.Combine(_env.WebRootPath, "uploads"),
+                BaseUrl = "/uploads"
+            };
+
+            var result = await _documentService.CreateDocument(documentRequestDTO, options);
+            return Ok(result);
+        }
+    }
+}
