cd371bf62e7c4ae02873b2bbead3ddffba0ee536
Eduardo J. Brenes Perez <edubpz08@gmail.com>
2026-04-02 19:30:35 -0600

    Adding Usuarios controller to public site.

---
+++ AsadaLisboaBackend/Areas/Cliente/Controllers/UsuariosController.cs
@@ -0,0 +1,30 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using AsadaLisboaBackend.Models.DTOs.User;
+using AsadaLisboaBackend.Models.DTOs.Shared;
+using AsadaLisboaBackend.ServiceContracts.Users;
+
+namespace AsadaLisboaBackend.Areas.Cliente.Controllers
+{
+    [ApiController]
+    [AllowAnonymous]
+    [Area("Cliente")]
+    [ApiVersion("1.0")]
+    [Route("api/[area]/[controller]")]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly IUsersGetterService _usersGetterService;
+
+        public UsuariosController(IUsersGetterService usersGetterService)
+        {
+            _usersGetterService = usersGetterService;
+        }
+
+        [HttpGet("")]
+        public async Task<ActionResult<PageResponseDTO<UserResponseDTO>>> GetUsers([FromQuery] SearchSortRequestDTO searchSortRequestDTO)
+        {
+            return Ok(await _usersGetterService.GetUsers(searchSortRequestDTO));
+        }
+    }
+}
