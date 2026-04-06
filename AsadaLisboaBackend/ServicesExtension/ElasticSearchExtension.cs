5c96516eb3df3564125c38ded05142cd2a624003
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-06 16:36:38 -0600

    Modified ElasticSearchExtension

--- AsadaLisboaBackend/ServicesExtension/ElasticSearchExtension.cs
+++ AsadaLisboaBackend/ServicesExtension/ElasticSearchExtension.cs
@@ -1,6 +1,22 @@
-﻿namespace AsadaLisboaBackend.ServicesExtension
+﻿using Microsoft.EntityFrameworkCore;
+using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
+
+namespace AsadaLisboaBackend.ServicesExtension
 {
-    public class ElasticSearchExtension
+    public static class ElasticSearchExtension
     {
+        public static IServiceCollection ElasticSearchRegistration(this IServiceCollection services, IConfiguration configuration)
+        {
+            var url = configuration["Elasticsearch:Url"];
+
+            var settings = new ElasticsearchClientSettings(new Uri(url))                    
+           .DefaultIndex("contenido"); 
+
+            var client = new ElasticsearchClient(settings);
+
+            return services.AddSingleton(client);
+        }
+
     }
 }
