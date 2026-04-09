b92416179c4b4ce87995c15e65ba582e5455713c
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-09 13:02:43 -0600

    Add Logging to StatusesGetterService

--- AsadaLisboaBackend.Services/Statuses/StatusesGetterService.cs
+++ AsadaLisboaBackend.Services/Statuses/StatusesGetterService.cs
@@ -1,21 +1,36 @@
 ﻿using AsadaLisboaBackend.Models.DTOs.Status;
 using AsadaLisboaBackend.ServiceContracts.Statuses;
 using AsadaLisboaBackend.RepositoryContracts.Statuses;
+using Microsoft.Extensions.Logging;
 
 namespace AsadaLisboaBackend.Services.Statuses
 {
     public class StatusesGetterService : IStatusesGetterService
     {
         private readonly IStatusesGetterRepository _statusesGetterRepository;
+        private readonly ILogger<StatusesGetterService> _logger;
 
-        public StatusesGetterService(IStatusesGetterRepository statusesGetterRepository)
+        public StatusesGetterService(IStatusesGetterRepository statusesGetterRepository, ILogger<StatusesGetterService> logger)
         {
             _statusesGetterRepository = statusesGetterRepository;
+            _logger = logger;
         }
 
         public async Task<List<StatusResponseDTO>> GetStatuses()
         {
-            return await _statusesGetterRepository.GetStatuses();
+            try
+            {
+                var result = await _statusesGetterRepository.GetStatuses();
+
+                _logger.LogInformation("Obtención exitosa de estados.");
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error al obtener los estados");
+                throw;
+            }
         }
     }
 }
