d6b775ab8947b38196d6fd2d76c86af6a0a0dd07
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-09 11:07:43 -0600

    Add Logging to ConfigurationsUpdaterService

--- AsadaLisboaBackend.Services/Configurations/ConfigurationsUpdaterService.cs
+++ AsadaLisboaBackend.Services/Configurations/ConfigurationsUpdaterService.cs
@@ -1,22 +1,32 @@
 ﻿using AsadaLisboaBackend.Models.DTOs.Configuration;
 using AsadaLisboaBackend.ServiceContracts.Configurations;
 using AsadaLisboaBackend.RepositoryContracts.Configurations;
+using Microsoft.Extensions.Logging;
 
 namespace AsadaLisboaBackend.Services.Configurations
 {
     public class ConfigurationsUpdaterService : IConfigurationsUpdaterService
     {
         private readonly IConfigurationsUpdaterRepository _configurationsUpdaterRepository;
+        private readonly ILogger<ConfigurationsUpdaterService> _logger;
 
-        public ConfigurationsUpdaterService(IConfigurationsUpdaterRepository configurationsUpdaterRepository)
+        public ConfigurationsUpdaterService(IConfigurationsUpdaterRepository configurationsUpdaterRepository, ILogger<ConfigurationsUpdaterService> logger)
         {
-            _configurationsUpdaterRepository = configurationsUpdaterRepository;
+            _configurationsUpdaterRepository = configurationsUpdaterRepository;            _logger = logger;
+
         }
 
         public async Task<ConfigurationResponseDTO> UpdateConfiguration(Guid id, ConfigurationsRequestDTO configurationRequestDTO)
         {
-            return (await _configurationsUpdaterRepository.UpdateConfiguration(id, configurationRequestDTO))
-                .ToConfigurationResponseDTO();
+           var result = await _configurationsUpdaterRepository.UpdateConfiguration(id, configurationRequestDTO);
+
+            if (result = null){
+                _logger.LogWarning("No se encontró configuración para actualizar con Id: {Id}", id);
+            }
+
+            _logger.LogInformation("Actualización exitosa de configuración con Id: {Id}", id);
+
+            return  result.ToConfigurationResponseDTO();
         }
     }
 }
