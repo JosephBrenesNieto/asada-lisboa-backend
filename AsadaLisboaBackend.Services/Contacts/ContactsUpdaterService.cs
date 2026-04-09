bbceac5fcbd422b4646c40910b77647af8bb5be1
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-09 12:13:14 -0600

    Add Logging to ContactsUpdaterService

--- AsadaLisboaBackend.Services/Contacts/ContactsUpdaterService.cs
+++ AsadaLisboaBackend.Services/Contacts/ContactsUpdaterService.cs
@@ -1,22 +1,33 @@
 ﻿using AsadaLisboaBackend.Models.DTOs.Contact;
 using AsadaLisboaBackend.ServiceContracts.Contacts;
 using AsadaLisboaBackend.RepositoryContracts.Contacts;
+using Microsoft.Extensions.Logging;
 
 namespace AsadaLisboaBackend.Services.Contacts
 {
     public class ContactsUpdaterService : IContactsUpdaterService
     {
         private readonly IContactsUpdaterRepository _contactsUpdaterRepository;
+        private readonly ILogger<ContactsUpdaterService> _logger;
 
-        public ContactsUpdaterService(IContactsUpdaterRepository contactsUpdaterRepository)
+        public ContactsUpdaterService(IContactsUpdaterRepository contactsUpdaterRepository, ILogger<ContactsUpdaterService> logger)
         {
             _contactsUpdaterRepository = contactsUpdaterRepository;
+            _logger = logger;
         }
 
         public async Task<ContactResponseDTO> UpdateContact(Guid id, ContactRequestDTO contactsRequestDTO)
         {
-            return (await _contactsUpdaterRepository.UpdateContact(id, contactsRequestDTO))
-                .ToContactResponseDTO();
+            var result = (await _contactsUpdaterRepository.UpdateContact(id, contactsRequestDTO));
+
+            if (result = null)
+            {
+                _logger.LogWarning("No se encontró contacto para actualizar con Id: {Id}", id);
+            }
+
+            _logger.LogInformation("Actualización exitosa de contacto con Id: {Id}", id);
+
+            return result.ToContactResponseDTO();
         }
     }
 }
