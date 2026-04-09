dae64c03bc93369037ada9dcdc59817022a37693
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-09 12:08:40 -0600

    Add Logging to ContactsAdderService

--- AsadaLisboaBackend.Services/Contacts/ContactsAdderService.cs
+++ AsadaLisboaBackend.Services/Contacts/ContactsAdderService.cs
@@ -1,30 +1,54 @@
 ﻿using AsadaLisboaBackend.Models;
+using AsadaLisboaBackend.Models.DTOs.Configuration;
 using AsadaLisboaBackend.Models.DTOs.Contact;
-using AsadaLisboaBackend.ServiceContracts.Contacts;
 using AsadaLisboaBackend.RepositoryContracts.Contacts;
+using AsadaLisboaBackend.ServiceContracts.Contacts;
+using Microsoft.Extensions.Logging;
 
 namespace AsadaLisboaBackend.Services.Contacts
 {
     public class ContactsAdderService : IContactsAdderService
     {
         private readonly IContactsAdderRepository _contactsAdderRepository;
+        private readonly ILogger<ContactsAdderService> _logger;
 
-        public ContactsAdderService(IContactsAdderRepository contactsAdderRepository)
+        public ContactsAdderService(IContactsAdderRepository contactsAdderRepository, ILogger<ContactsAdderService> logger)
         {
             _contactsAdderRepository = contactsAdderRepository;
+            _logger = logger;
         }
 
         public async Task<ContactResponseDTO> CreateContact(ContactRequestDTO contactRequestDTO)
         {
-            var contact = new Contact()
-            {
-                Order = contactRequestDTO.Order,
-                Value = contactRequestDTO.Value,
-                ContactType = contactRequestDTO.ContactType,
-            };
+            try { 
+                var contact = new Contact()
+                {
+                    Order = contactRequestDTO.Order,
+                    Value = contactRequestDTO.Value,
+                    ContactType = contactRequestDTO.ContactType,
+                };
 
-            return (await _contactsAdderRepository.CreateContact(contact))
-                .ToContactResponseDTO();
+                var result = (await _contactsAdderRepository.CreateContact(contact));
+
+                _logger.LogInformation(
+                  "Contacto creado exitosamente. Tipo: {ContactType}, Orden: {Order}",
+                  contactRequestDTO.ContactType,
+                  contactRequestDTO.Order
+              );
+
+                return result.ToContactResponseDTO();
+            
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex,
+                    "Error al crear contacto. Tipo: {ContactType}, Orden: {Order}",
+                    contactRequestDTO.ContactType,
+                    contactRequestDTO.Order
+                );
+                throw;
+            }
+
         }
     }
 }
