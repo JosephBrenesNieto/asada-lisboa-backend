e63af2954a1555cdba4cbbddea638f7ad3571cef
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-09 08:13:01 -0600

    Add Logging to ChargesGetterService

--- AsadaLisboaBackend.Services/Charges/ChargesGetterService.cs
+++ AsadaLisboaBackend.Services/Charges/ChargesGetterService.cs
@@ -1,21 +1,33 @@
 ﻿using AsadaLisboaBackend.Models.DTOs.Charge;
 using AsadaLisboaBackend.ServiceContracts.Charges;
 using AsadaLisboaBackend.RepositoryContracts.Charges;
+using Microsoft.Extensions.Logging;
+using AsadaLisboaBackend.Services.Exceptions;
 
 namespace AsadaLisboaBackend.Services.Charges
 {
     public class ChargesGetterService : IChargesGetterService
     {
         private readonly IChargesGetterRepository _chargesGetterRepository;
+        private readonly ILogger<ChargesUpdaterService> _logger;
 
-        public ChargesGetterService(IChargesGetterRepository chargesGetterRepository)
+        public ChargesGetterService(IChargesGetterRepository chargesGetterRepository, ILogger<ChargesUpdaterService> logger)
         {
             _chargesGetterRepository = chargesGetterRepository;
+            _logger = logger;
         }
 
         public async Task<List<ChargeResponseDTO>> GetCharges()
         {
-            return await _chargesGetterRepository.GetCharges();
+            try
+            {
+                return await _chargesGetterRepository.GetCharges();
+
+            } catch (Exception ex) 
+            {
+                _logger.LogError(ex, "Ocurrió un error al intentar obtener la lista de cargos.");
+                throw new CreateObjectException("Error al obtener la lista de cargos.");
+            }
         }
 
         public async Task<bool> ExistsCharge(string name)
