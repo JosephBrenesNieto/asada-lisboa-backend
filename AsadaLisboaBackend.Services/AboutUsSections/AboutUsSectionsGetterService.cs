fe98e3ec5b7c6f311dc99ce5bddd9271f5868e8b
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-09 09:52:11 -0600

    Add Logging to AboutUsSectionsGetterService

--- AsadaLisboaBackend.Services/AboutUsSections/AboutUsSectionsGetterService.cs
+++ AsadaLisboaBackend.Services/AboutUsSections/AboutUsSectionsGetterService.cs
@@ -2,23 +2,46 @@
 using AsadaLisboaBackend.Models.DTOs.AboutUs;
 using AsadaLisboaBackend.ServiceContracts.AboutUsSections;
 using AsadaLisboaBackend.RepositoryContracts.AboutUsSections;
+using Microsoft.Extensions.Logging;
 
 namespace AsadaLisboaBackend.Services.AboutUsSections
 {
     public class AboutUsSectionsGetterService : IAboutUsSectionsGetterService
     {
         private readonly IAboutUsSectionsGetterRepository _aboutUsSectionsGetterRepository;
+        private readonly ILogger<AboutUsSectionsGetterService> _logger;
 
-        public AboutUsSectionsGetterService(IAboutUsSectionsGetterRepository aboutUsSectionsGetterRepository)
+        public AboutUsSectionsGetterService(IAboutUsSectionsGetterRepository aboutUsSectionsGetterRepository, ILogger<AboutUsSectionsGetterService> logger)
         {
             _aboutUsSectionsGetterRepository = aboutUsSectionsGetterRepository;
+            _logger = logger;
         }
 
         public async Task<PageResponseDTO<AboutUsResponseDTO>> GetAboutUsSections(SearchSortRequestDTO searchSortRequestDTO)
         {
-            searchSortRequestDTO.Offset = (Math.Max(searchSortRequestDTO.Page, 1) - 1) * searchSortRequestDTO.Take;
+            try
+            {
+                searchSortRequestDTO.Offset = (Math.Max(searchSortRequestDTO.Page, 1) - 1) * searchSortRequestDTO.Take;
 
-            return await _aboutUsSectionsGetterRepository.GetAboutUsSections(searchSortRequestDTO);
+               var result = await _aboutUsSectionsGetterRepository.GetAboutUsSections(searchSortRequestDTO);
+
+                _logger.LogInformation(
+                    "Obtención exitosa de AboutUsSections. Página: {Page}, Tamaño: {Take}",
+                    searchSortRequestDTO.Page,
+                    searchSortRequestDTO.Take
+                );
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex,
+                    "Error al obtener AboutUsSections. Página: {Page}, Tamaño: {Take}",
+                    searchSortRequestDTO.Page,
+                    searchSortRequestDTO.Take
+                );
+                throw;
+            }
         }
     }
 }
