188ab4fff2ae115e451ecbce9c31716e2f91a6eb
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-09 15:19:58 -0600

    Add Caching to AboutUsSectionsUpdaterService

--- AsadaLisboaBackend.Services/AboutUsSections/AboutUsSectionsUpdaterService.cs
+++ AsadaLisboaBackend.Services/AboutUsSections/AboutUsSectionsUpdaterService.cs
@@ -2,6 +2,8 @@
 using AsadaLisboaBackend.ServiceContracts.AboutUsSections;
 using AsadaLisboaBackend.RepositoryContracts.AboutUsSections;
 using Microsoft.Extensions.Logging;
+using AsadaLisboaBackend.ServiceContracts.MemoryCaches;
+using AsadaLisboaBackend.Utils;
 
 namespace AsadaLisboaBackend.Services.AboutUsSections
 {
@@ -9,11 +11,13 @@ namespace AsadaLisboaBackend.Services.AboutUsSections
     {
         private readonly IAboutUsSectionsUpdaterRepository _aboutUsSectionsUpdaterRepository;
         private readonly ILogger<AboutUsSectionsUpdaterService> _logger;
+        private readonly IMemoryCachesService _memoryCachesService;
 
-        public AboutUsSectionsUpdaterService(IAboutUsSectionsUpdaterRepository aboutUsSectionsUpdaterRepository, ILogger<AboutUsSectionsUpdaterService> logger)
+        public AboutUsSectionsUpdaterService(IAboutUsSectionsUpdaterRepository aboutUsSectionsUpdaterRepository, ILogger<AboutUsSectionsUpdaterService> logger, IMemoryCachesService memoryCachesService)
         {
             _aboutUsSectionsUpdaterRepository = aboutUsSectionsUpdaterRepository;
             _logger = logger;
+            _memoryCachesService = memoryCachesService;
         }
 
         public async Task<AboutUsResponseDTO> UpdateAboutUsSection(Guid id, AboutUsRequestDTO aboutUsRequestDTO)
@@ -27,7 +31,11 @@ namespace AsadaLisboaBackend.Services.AboutUsSections
 
             _logger.LogInformation("Actualización exitosa de AboutUsSection con Id: {Id}", id);
 
+            _memoryCachesService.RemoveById(Constants.CACHE_ABOUT_US, id);
+            _memoryCachesService.ChangeVersion(Constants.CACHE_ABOUT_US);
+
             return result.ToAboutUsResponseDTO();
+            
         }
     }
 }
