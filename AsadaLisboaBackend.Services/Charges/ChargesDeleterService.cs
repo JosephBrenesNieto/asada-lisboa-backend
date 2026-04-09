8c426091d621d98fa6353aae26746a9307743100
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-09 16:50:02 -0600

    Add Caching to ChargesDeleterService

--- AsadaLisboaBackend.Services/Charges/ChargesDeleterService.cs
+++ AsadaLisboaBackend.Services/Charges/ChargesDeleterService.cs
@@ -4,7 +4,8 @@ using AsadaLisboaBackend.ServiceContracts.Charges;
 using AsadaLisboaBackend.Services.Exceptions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
-
+using AsadaLisboaBackend.ServiceContracts.MemoryCaches;
+using AsadaLisboaBackend.Utils;
 
 namespace AsadaLisboaBackend.Services.Charges
 {
@@ -13,12 +14,14 @@ namespace AsadaLisboaBackend.Services.Charges
         private readonly IChargesDeleterRepository _chargesDeleterRepository;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<ChargesDeleterService> _logger;
+        private readonly IMemoryCachesService _memoryCachesService;
 
-        public ChargesDeleterService(IChargesDeleterRepository chargesDeleterRepository, UserManager<ApplicationUser> userManager, ILogger<ChargesDeleterService> logger)
+        public ChargesDeleterService(IChargesDeleterRepository chargesDeleterRepository, UserManager<ApplicationUser> userManager, ILogger<ChargesDeleterService> logger, IMemoryCachesService memoryCachesService)
         {
             _chargesDeleterRepository = chargesDeleterRepository;
             _userManager = userManager;
             _logger = logger;
+            _memoryCachesService = memoryCachesService;
         }
 
         public async Task DeleteCharge(Guid id)
@@ -32,6 +35,10 @@ namespace AsadaLisboaBackend.Services.Charges
             }
 
             await _chargesDeleterRepository.DeleteCharge(id);
+
+            _memoryCachesService.RemoveById(Constants.CACHE_CHARGES, id);
+            _memoryCachesService.ChangeVersion(Constants.CACHE_CHARGES);
+
 
             _logger.LogInformation("Cargo con id {ChargeId} eliminado exitosamente.", id);
 
