4826c5b87a0718e23768b254e5df4383f69a2907
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-09 12:10:21 -0600

    Add Logging to ContactsDeleterService

--- AsadaLisboaBackend.Services/Contacts/ContactsDeleterService.cs
+++ AsadaLisboaBackend.Services/Contacts/ContactsDeleterService.cs
@@ -1,20 +1,36 @@
 ﻿using AsadaLisboaBackend.ServiceContracts.Contacts;
 using AsadaLisboaBackend.RepositoryContracts.Contacts;
+using Microsoft.Extensions.Logging;
 
 namespace AsadaLisboaBackend.Services.Contacts
 {
     public class ContactsDeleterService : IContactsDeleterService
     {
         private readonly IContactsDeleterRepository _contactsDeleterRepository;
+        private readonly ILogger<ContactsDeleterService> _logger;
 
-        public ContactsDeleterService(IContactsDeleterRepository contactsDeleterRepository)
+        public ContactsDeleterService(IContactsDeleterRepository contactsDeleterRepository, ILogger<ContactsDeleterService> logger)
         {
             _contactsDeleterRepository = contactsDeleterRepository;
+            _logger = logger;
         }
 
         public async Task DeleteContact(Guid id)
         {
-            await _contactsDeleterRepository.DeleteContact(id);
+            try { 
+
+                await _contactsDeleterRepository.DeleteContact(id);
+
+                _logger.LogInformation("Contacto con Id: {Id} eliminado exitosamente.", id);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error eliminar  contacto con Id: {Id}", id);
+                throw;
+            }
+
+
         }
     }
 }
